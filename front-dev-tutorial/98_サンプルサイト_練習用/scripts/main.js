ac667d0107bdeaf0e3e71b9680280c3ea26afdca
Rion <rion.it456@gmail.com>
2022-03-25 00:43:26 +0900

    web application development last half(part 2)

--- front-dev-tutorial/98_サンプルサイト_練習用/scripts/main.js
+++ front-dev-tutorial/98_サンプルサイト_練習用/scripts/main.js
@@ -1,21 +1,93 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const hero = new HeroSlider('.swiper');
-  hero.start({delay: 2000});
+  const main = new Main();
+});
 
-  const cb = function(el, inview) {
+class Main {
+  constructor() {
+    this.header = document.querySelector('.header');
+    this.sides = document.querySelectorAll('.side');
+    this._observers = [];
+    this._init();
+  }
+
+  set observers(val) {
+    this._observers.push(val);
+  }
+
+  get observers() {
+    return this._observers;
+  }
+
+  _init() {
+    new MobileMenu();
+    this.hero = new HeroSlider('.swiper');
+    Pace.on('done', this._paceDone.bind(this));
+  }
+  
+  _paceDone() {
+    this._scrollInit();
+  }
+
+  _textAnimation(el, inview) {
     if(inview) {
         const ta = new TweenTextAnimation(el);
         ta.animate();
     }
   }
-  const so = new ScrollObserver('.tween-animate-title', cb);
 
-  const _inviewAnimation = function(el, inview) {
+  _navAnimation(el, inview) {
+    if(inview) {
+      this.header.classList.remove('triggered');
+    } else {
+      this.header.classList.add('triggered');
+    }
+  }
+
+  _sideAnimation(el, inview) {
+    if(inview) {
+      this.sides.forEach(side => {
+        side.classList.add('inview');
+      });
+    } else {
+      this.sides.forEach(side => {
+        side.classList.remove('inview');
+      });
+    }
+  }
+
+  _inviewAnimation(el, inview) {
     if(inview) {
       el.classList.add('inview');
     } else {
       el.classList.remove('inview');
     }
   }
-  const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
-});
+
+  _toggleSlideAnimation(el, inview) {
+    if(inview) {
+      this.hero.start();
+    } else {
+      this.hero.stop();
+    }
+  }
+
+  _destroyObserver() {
+    this.observers.forEach((ob) => {
+      ob.destroy();
+    })
+  }
+
+  destroy() {
+    this._destroyObserver();
+  }
+
+  _scrollInit() {
+    this.observers = new ScrollObserver('.nav-trigger', this._navAnimation.bind(this), {once: false});
+    this.observers = new ScrollObserver('.cover-slide', this._inviewAnimation);
+    this.observers = new ScrollObserver('.tween-animate-title', this._textAnimation)
+    this.observers = new ScrollObserver('.swiper', this._toggleSlideAnimation.bind(this), {once: false})
+    this.observers = new ScrollObserver('.appear', this._inviewAnimation.bind(this), {once: false})
+    this.observers = new ScrollObserver('#main-content', this._sideAnimation.bind(this), {once: false, rootMargin: "-300px 0px"})
+    console.log(this.observers);
+  }
+}
